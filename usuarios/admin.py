4e220e452e1945f752563520125f9411081810df
German Robayo Paz <germanrobayo33@gmail.com>
2018-08-28 07:54:29 -0400

    anade admin para usuarios personalizados

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UsuarioCreationForm, UsuarioChangeForm
+from .models import Usuario
+
+class UsuarioAdmin(UserAdmin):
+    add_form = UsuarioCreationForm
+    form = UsuarioChangeForm
+    model = Usuario
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(Usuario, UsuarioAdmin)
