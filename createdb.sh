6de954c7cbc563a8a264612efcbf37e8301291b0
German Robayo Paz <germanrobayo33@gmail.com>
2018-08-28 07:40:38 -0400

    anade script para crear la base de datos local

---
+++ createdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo -u postgres dropdb ceic_site >> /dev/null
+sudo -u postgres dropuser ceicsite >> /dev/null
+sudo -u postgres createuser ceicsite
+sudo -u postgres createdb ceic_site
+sudo -u postgres psql << EOF
+alter user ceicsite with encrypted password 'ceicsite';
+alter role ceicsite set client_encoding to 'utf8';
+alter role ceicsite set default_transaction_isolation to 'read committed';
+alter role ceicsite set timezone to 'utc';
+grant all privileges on database ceic_site to ceicsite ;\q
+EOF
