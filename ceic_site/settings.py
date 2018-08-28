872e5873eb6db833b631a013d57bb1ad81d33565
German Robayo Paz <germanrobayo33@gmail.com>
2018-08-28 00:57:22 -0400

    anade release target y configuracion de base de datos

--- ceic_site/settings.py
+++ ceic_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ceic_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'ceic_site'),
+        'USER': os.environ.get('DB_USER', 'ceicsite'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'ceicsite'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': '',
     }
 }
 
