9bf41e2343e2c99420bf0539db82e91b0841a30f
German Robayo Paz <germanrobayo33@gmail.com>
2018-08-28 07:41:45 -0400

    anade modelo personalizado de User

--- ceic_site/settings.py
+++ ceic_site/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'usuarios.Usuario'
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,17 @@
+"""
+Modelos
+"""
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Usuario(AbstractUser):
+    """
+    Tabla de usuarios de ceic_site
+
+    Los docs de django siempre recomiendan usar un modelos de Usuario
+    personalizado en vez del default. Asi no se aniadan mas campos despues
+    """
+
+    def __str__(self):
+        return self.username
