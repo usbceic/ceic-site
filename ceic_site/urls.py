f146ff57c0b1e1eed1b164c17bb7b6606c216b14
German Robayo Paz <germanrobayo33@gmail.com>
2018-10-22 23:14:46 -0400

    fix: modificar urlpatterns para que sirva los archivos estaticos

--- ceic_site/urls.py
+++ ceic_site/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 
 urlpatterns = [ # pylint: disable=invalid-name
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
