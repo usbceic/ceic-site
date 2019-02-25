363fd7efd53f7b36325ca066c39f4df520a73315
Arturo Yepez <arturyepez10@gmail.com>
2019-02-24 21:08:13 -0400

    se arregla expresión RegEx

--- ceic_site/validators.py
+++ ceic_site/validators.py
@@ -4,6 +4,7 @@ Validadores
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
+# Validador de clase Regex que se encarga de verificar patrones de Carnets Estudiantiles
 validate_carnet = RegexValidator(
-	r'[0-9-]',
-	message= "El Carnet Estudiantil debe contener solo números y guión.")
+    r'^[0-9]{2}-[0-9]{5}$',
+    message= "El Carnet Estudiantil debe contener solo números y guión.")
